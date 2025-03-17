279c1df47db68d87454d83f9b0f610924abc1218
wdjohanwd <wdjohanwd@gmail.com>
2025-03-17 14:15:32 +0100

    Creación de aplicación SpringBoot

---
+++ backend/culturefit/src/main/java/com/culturefit/culturefit/CulturefitApplication.java
@@ -0,0 +1,13 @@
+package com.culturefit.culturefit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulturefitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulturefitApplication.class, args);
+	}
+
+}
---
+++ backend/culturefit/src/test/java/com/culturefit/culturefit/CulturefitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.culturefit.culturefit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulturefitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
