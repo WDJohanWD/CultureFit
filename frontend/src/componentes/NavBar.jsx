4735adf444a13ee69d8abb2335eeaaef7b7b3f78
EstebanGP <estebangp2003@gmail.com>
2025-03-17 15:02:02 +0100

    Front listo para trabajar

---
+++ frontend/src/componentes/NavBar.jsx
@@ -0,0 +1,9 @@
+function NavBar() {
+  return (
+    <nav className="sticky shadow-lg flex items-center relative z-50 bg-white p-1 top-0">
+      <h2>Culture Fit</h2>
+    </nav>
+  );
+}
+
+export default NavBar;
