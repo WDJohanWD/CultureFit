c8e4fae1a1d09eee63c815812d18c52346e3ffa1
Borja <borjabm.e@gmail.com>
2025-03-22 04:53:16 +0100

    ⚙️ Cambios y eliminación de metodos para la pasarela de pagos

--- backend/culturefit/src/main/java/com/culturefit/culturefit/payments/controller/PaymentController.java
+++ backend/culturefit/src/main/java/com/culturefit/culturefit/payments/controller/PaymentController.java
@@ -4,19 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import com.culturefit.culturefit.payments.domain.CreateCustomerRequest;
-import com.culturefit.culturefit.payments.domain.CreateSubscriptionRequest;
 import com.culturefit.culturefit.payments.service.PaymentService;
 import com.stripe.exception.StripeException;
-import com.stripe.model.Customer;
-import com.stripe.model.Price;
-import com.stripe.model.Subscription;
-import com.stripe.model.checkout.Session;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import com.stripe.model.checkout.Session;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,28 +22,6 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
-    @PostMapping("/create-customer")
-    public ResponseEntity<?> createCustomer(@RequestBody CreateCustomerRequest request) throws StripeException {
-
-        Customer customer = paymentService.createCustomer(request);
-        Map<String, Object> response = new HashMap<>();
-        response.put("id", customer.getId());
-        response.put("name", customer.getName());
-        response.put("email", customer.getEmail());
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
-    }
-
-    @PostMapping("/create-subscription")
-    public ResponseEntity<?> createSubscription(@RequestBody CreateSubscriptionRequest request) throws StripeException {
-
-        Subscription subscription = paymentService.createSubscription(request);
-        Map<String, Object> response = new HashMap<>();
-        response.put("id", subscription.getId());
-        response.put("customerId", subscription.getCustomer());
-        response.put("priceId", subscription.getItems().getData().get(0).getPrice().getId());
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
-    }
-
     @PostMapping("/create-checkout-session/{priceId}")
     public ResponseEntity<?> createCheckoutSession(@PathVariable String priceId) throws StripeException {
         Session session = paymentService.createCheckoutSession(priceId);
@@ -59,32 +30,4 @@ public class PaymentController {
         response.put("url", session.getUrl());
         return ResponseEntity.ok(response);
     }
-    
-
-    //TODO: Completar los siguientes endpoints
-    // @GetMapping("/invoice-preview")
-    // public ResponseEntity<?> invoicePreview(@RequestParam String customerId, @RequestParam String newPriceId, @RequestParam String subscriptionId) throws StripeException {
-    //     Map<String, Object> response = paymentService.getInvoicePreview(customerId, newPriceId, subscriptionId);
-    //     return ResponseEntity.ok(response);
-    // }
-
-    // @PostMapping("/cancel-subscription")
-    // public ResponseEntity<?> cancelSubscription(@RequestBody CancelSubscriptionRequest request) throws StripeException {
-    //     Subscription subscription = paymentService.cancelSubscription(request.getSubscriptionId());
-    //     Map<String, Object> response = new HashMap<>();
-    //     response.put("subscription", subscription);
-    //     return ResponseEntity.ok(response);
-    // }
-
-    // @PostMapping("/update-subscription")
-    // public ResponseEntity<?> updateSubscription(@RequestBody UpdateSubscriptionRequest request) throws StripeException {
-    //     Map<String, Object> response = paymentService.updateSubscription(request);
-    //     return ResponseEntity.ok(response);
-    // }
-
-
-    // @PostMapping("/webhook")
-    // public ResponseEntity<?> handleWebhook(@RequestBody String payload, @RequestHeader("Stripe-Signature") String sigHeader) {
-    //     return paymentService.handleWebhook(payload, sigHeader);
-    // }
 }
--- backend/culturefit/src/main/java/com/culturefit/culturefit/payments/service/PaymentService.java
+++ backend/culturefit/src/main/java/com/culturefit/culturefit/payments/service/PaymentService.java
@@ -1,52 +1,16 @@
 package com.culturefit.culturefit.payments.service;
 
-import java.util.Arrays;
-
 import org.springframework.stereotype.Service;
 
-import com.culturefit.culturefit.payments.domain.CreateCustomerRequest;
-import com.culturefit.culturefit.payments.domain.CreateSubscriptionRequest;
-import com.stripe.exception.SignatureVerificationException;
 import com.stripe.exception.StripeException;
-import com.stripe.model.Customer;
-import com.stripe.model.Event;
-import com.stripe.model.Invoice;
-import com.stripe.model.Subscription;
 import com.stripe.model.checkout.Session;
-import com.stripe.net.Webhook;
-import com.stripe.param.CustomerCreateParams;
-import com.stripe.param.InvoiceUpcomingParams;
-import com.stripe.param.SubscriptionCreateParams;
 import com.stripe.param.checkout.SessionCreateParams;
 
 
 @Service
 public class PaymentService {
-    
-    public Customer createCustomer(CreateCustomerRequest request) throws StripeException {
-        CustomerCreateParams params = CustomerCreateParams.builder()
-            .setName(request.getName())
-            .setEmail(request.getEmail())
-            .build();
-
-        return Customer.create(params);
-        
-    }
-
-    public Subscription createSubscription(CreateSubscriptionRequest request) throws StripeException {
-        SubscriptionCreateParams params = SubscriptionCreateParams.builder()
-            .setCustomer(request.getCustomerId())
-            .addItem(SubscriptionCreateParams.Item.builder().setPrice(request.getPriceId()).build())
-            .setPaymentBehavior(SubscriptionCreateParams.PaymentBehavior.DEFAULT_INCOMPLETE)
-            .addAllExpand(Arrays.asList("latest_invoice.payment_intent"))
-            .build();
-
-        return Subscription.create(params);
-    }
-
     public Session createCheckoutSession(String priceId) throws StripeException {
         SessionCreateParams params = SessionCreateParams.builder()
-            //TODO: Cambiar las urls por unas válidas
             .setSuccessUrl("https://example.com/success")
             .setCancelUrl("https://example.com/cancel")
             .addLineItem(
@@ -59,50 +23,5 @@ public class PaymentService {
             .build();
 
         return Session.create(params);
-    }
-
-
-    // TODO: Realizar metodos de cancelación y actualizacion de subscripcion
-    // public Subscription cancelSubscription(String subscriptionId) throws StripeException {
-    //     Subscription subscription = Subscription.retrieve(subscriptionId);
-    //     return subscription.cancel();
-    // }
-
-    // public Subscription updateSubscription(String subscriptionId, String newPriceId) throws StripeException {
-    //     Subscription subscription = Subscription.retrieve(subscriptionId);
-    //     SubscriptionUpdateParams params = SubscriptionUpdateParams.builder()
-    //             .addItem(SubscriptionUpdateParams.Item.builder()
-    //                     .setId(subscription.getItems().getData().get(0).getId())
-    //                     .setPrice(newPriceId)
-    //                     .build())
-    //             .setCancelAtPeriodEnd(false)
-    //             .build();
-    //     return subscription.update(params);
-    // }
-    
-    public Invoice getInvoicePreview(String subscriptionId) throws StripeException {
-        Subscription subscription = Subscription.retrieve(subscriptionId);
-
-        InvoiceUpcomingParams params = InvoiceUpcomingParams.builder()
-                .setCustomer(subscription.getCustomer())
-                .setSubscription(subscriptionId)
-                .addSubscriptionItem(InvoiceUpcomingParams.SubscriptionItem.builder()
-                        .setId(subscription.getItems().getData().get(0).getId())
-                        .setPrice(subscription.getItems().getData().get(0).getPrice().getId())
-                        .build())
-                .build();
-        return Invoice.upcoming(params);
-    }
-
-    public String handleWebhook(String payload, String sigHeader) {
-        String endpointSecret = "your_webhook_secret";
-        Event event;
-        try {
-            event = Webhook.constructEvent(payload, sigHeader, endpointSecret);
-            System.out.println(event.getObject());
-        } catch (SignatureVerificationException e) {
-            return "Invalid signature";
-        }
-        return "Webhook received";
     }
 }
