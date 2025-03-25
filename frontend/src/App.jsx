40673d95624ffec5d1c963a316fa615478b3f0e4
Johan Fernando Aponte Valencia <wdjohanwd@gmail.com>
2025-03-25 11:23:31 +0100

    🎨 Pagina 404, hecha.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import NavBar from "./componentes/NavBar";
 import './index.css';
 import PaginaInicio from "./secciones/PaginaInicio";
 import PaginaPlanes from "./secciones/PaginaPlanes";
+import Pagina404 from "./secciones/Pagina404";
 
 function Layout() {
   return (
     <>
       <NavBar />
       <Routes>
+        <Route path="*" element={<Pagina404/>}/>
         <Route path="/" element={<PaginaInicio />} />
         <Route path="/planes" element={<PaginaPlanes />} />
       </Routes>
