8f9b31806b803d4865d1994023ce24480d14cd87
Johan Fernando Aponte Valencia <wdjohanwd@gmail.com>
2025-03-25 14:46:54 +0100

    🎨 Eliminada página 404, debido a que ya fue creada

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,12 @@ import NavBar from "./componentes/NavBar";
 import './index.css';
 import PaginaInicio from "./secciones/PaginaInicio";
 import PaginaPlanes from "./secciones/PaginaPlanes";
-import Pagina404 from "./secciones/Pagina404";
 
 function Layout() {
   return (
     <>
       <NavBar />
       <Routes>
-        <Route path="*" element={<Pagina404/>}/>
         <Route path="/" element={<PaginaInicio />} />
         <Route path="/planes" element={<PaginaPlanes />} />
       </Routes>
