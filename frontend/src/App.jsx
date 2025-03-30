e7035fa5ffe832786b5feb709d7516d1409f2f22
Johan Fernando Aponte Valencia <wdjohanwd@gmail.com>
2025-03-30 12:17:24 +0200

    🎨 Admin template created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Signup from "./sections/Signup"
 import Login from "./sections/Login"
 import AboutUs from "./sections/AboutUs";
 import ConfirmAccount from "./sections/ConfirmAccount";
+import Administration from "./sections/Admin/Administration";
 
 function Layout() {
 
@@ -23,6 +24,8 @@ function Layout() {
         <Route path="/" element={<Home />} />
         <Route path="/memberships" element={<Memberships />} />
         <Route path="/aboutus" element={<AboutUs />}/>
+        <Route path="/admin" element={<Administration />}/>
+        
       </Routes>
     </>
   );
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import compression from 'vite-plugin-compression';
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -22,4 +21,11 @@ export default defineConfig({
       }
     )
   ],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@/components/ui': '/src/components/ui' 
+
+    }
+  }
 })
