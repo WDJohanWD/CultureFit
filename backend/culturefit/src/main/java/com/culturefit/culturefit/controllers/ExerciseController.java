d34853d499121c18006d56f7c9649a620e949109
Esteban Gonzalez <estebangp2003@gmail.com>
2025-04-10 11:16:20 +0200

    Controller para ejercicios

--- backend/culturefit/src/main/java/com/culturefit/culturefit/controllers/ExerciseController.java
+++ backend/culturefit/src/main/java/com/culturefit/culturefit/controllers/ExerciseController.java
@@ -1,8 +1,60 @@
 package com.culturefit.culturefit.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.culturefit.culturefit.domains.Exercise;
+import com.culturefit.culturefit.services.exerciseService.ExerciseService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 @Controller
 public class ExerciseController {
-    //Gets
+    @Autowired
+    ExerciseService exerciseService;
+
+    // Getters
+    @GetMapping ("/exercise")
+    public List<Exercise> getExercises(){
+        List<Exercise> exercises = exerciseService.getExercise();
+        return exercises;
+    }
+
+     @GetMapping("/exercise/{id}")
+    public ResponseEntity<Exercise> getExercise(@PathVariable Long id) {
+        Exercise exercise = exerciseService.getExercise(id);
+        return ResponseEntity.ok(exercise);
+    }
+
+    // Posts
+    @PostMapping("/new-exercise")
+    public Exercise newExercise (@Valid @RequestBody Exercise exercise) {
+        Exercise exerciseBD = exerciseService.saveExercise(exercise);
+        return exerciseBD;
+    }
+
+    //Delete
+    @DeleteMapping ("/delete-exercise/{id}")
+    public void deleteExercise(Long id) {
+        exerciseService.deleteExercise(id);
+    }
+
+    // Put
+    @PutMapping("/edit-exercise")
+    public Exercise editExercise(@Valid @RequestBody Exercise exercise) {
+        Exercise updatedExercise = exerciseService.updateExercise(exercise);
+        return updatedExercise;
+    }
 }
