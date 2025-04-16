62b5d9317294226a7e7fc7b81759f4e445c5232e
Johan Fernando Aponte Valencia <wdjohanwd@gmail.com>
2025-04-16 10:22:21 +0200

    Idiomas

--- frontend/src/sections/Profile.jsx
+++ frontend/src/sections/Profile.jsx
@@ -1,3 +1,4 @@
+import { useTranslation } from "react-i18next"
 import { useContext, useEffect, useState } from "react"
 import { AuthContext } from "../AuthContext"
 import { User, Lock, Calendar, Mail, CreditCard, Camera } from "lucide-react"
@@ -14,7 +15,7 @@ import { toast } from "@/components/ui/use-toast"
 
 export default function Profile() {
     const { user: authUser, loading } = useContext(AuthContext)
-
+    const { t } = useTranslation("profile")
     const API_URL = "http://localhost:9000"
     const [isEditing, setIsEditing] = useState(false)
     const [user, setUser] = useState(null)
@@ -127,26 +128,29 @@ export default function Profile() {
     return (
         <div className="container mx-auto py-10">
             <div className="container mx-auto py-10">
-                <h1 className="text-3xl font-bold mb-6">Mi Perfil</h1>
+                <h1 className="text-3xl font-bold mb-6">{t("title")}</h1>
 
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                     {/* Profile Image Section */}
                     <Card className="md:col-span-1">
                         <CardHeader>
-                            <CardTitle>Imagen de Perfil</CardTitle>
+                            <CardTitle>{t("text-image")}</CardTitle>
                         </CardHeader>
                         <CardContent className="flex flex-col items-center">
                             <Avatar className="h-32 w-32 mb-4">
                                 <AvatarImage src={isEditing ? `${API_URL}${formData.imageUrl}` : `${API_URL}${user.imageUrl}`} alt="Foto de perfil" />
-                                <AvatarFallback>{user.name.charAt(0)}</AvatarFallback>
+                                <AvatarFallback>{user.name}</AvatarFallback>
                             </Avatar>
+                            <p className="text-sm text-muted-foreground">{user.name}</p>
+                            <p className="text-sm text-muted-foreground">{user.email}</p>
+                            <p className="text-sm text-muted-foreground">{user.birthDate}</p>
 
                             {isEditing && (
                                 <div className="w-full">
                                     <Label htmlFor="profile-image" className="cursor-pointer">
                                         <div className="flex items-center justify-center gap-2 p-2 border-2 border-dashed rounded-md hover:bg-muted">
                                             <Camera className="h-4 w-4" />
-                                            <span>Cambiar imagen</span>
+                                            <span>{t("change-image")}</span>
                                         </div>
                                         <Input
                                             id="profile-image"
@@ -165,16 +169,16 @@ export default function Profile() {
                     <div className="md:col-span-2">
                         <Tabs defaultValue="info">
                             <TabsList className="grid w-full grid-cols-2">
-                                <TabsTrigger value="info">Información Personal</TabsTrigger>
-                                <TabsTrigger value="security">Seguridad</TabsTrigger>
+                                <TabsTrigger value="info">{t("tab1")}</TabsTrigger>
+                                <TabsTrigger value="security">{t("tab2")}</TabsTrigger>
                             </TabsList>
 
                             {/* Personal Information Tab */}
                             <TabsContent value="info">
                                 <Card>
                                     <CardHeader>
-                                        <CardTitle>Información Personal</CardTitle>
-                                        <CardDescription>Gestiona tu información personal y de contacto.</CardDescription>
+                                        <CardTitle>{t("tab1-tittle")}</CardTitle>
+                                        <CardDescription>{t("tab1-description")}</CardDescription>
                                     </CardHeader>
                                     <CardContent>
                                         <form onSubmit={handleProfileUpdate}>
@@ -183,7 +187,7 @@ export default function Profile() {
                                                     <div className="space-y-2">
                                                         <Label htmlFor="name" className="flex items-center gap-2">
                                                             <User className="h-4 w-4" />
-                                                            Nombre Completo
+                                                            {t("tab1-name")}
                                                         </Label>
                                                         <Input
                                                             id="name"
@@ -197,7 +201,7 @@ export default function Profile() {
                                                     <div className="space-y-2">
                                                         <Label htmlFor="email" className="flex items-center gap-2">
                                                             <Mail className="h-4 w-4" />
-                                                            Correo Electrónico
+                                                            {t("tab1-email")}
                                                         </Label>
                                                         <Input
                                                             id="email"
@@ -214,7 +218,7 @@ export default function Profile() {
                                                     <div className="space-y-2">
                                                         <Label htmlFor="birthDate" className="flex items-center gap-2">
                                                             <Calendar className="h-4 w-4" />
-                                                            Fecha de Nacimiento
+                                                            {t("tab1-birthDate")}
                                                         </Label>
                                                         <Input
                                                             id="birthDate"
@@ -229,7 +233,7 @@ export default function Profile() {
                                                     <div className="space-y-2">
                                                         <Label htmlFor="dni" className="flex items-center gap-2">
                                                             <CreditCard className="h-4 w-4" />
-                                                            DNI
+                                                            {t("tab1-dni")}
                                                         </Label>
                                                         <Input
                                                             id="dni"
@@ -252,13 +256,13 @@ export default function Profile() {
                                                                     setFormData({ ...user })
                                                                 }}
                                                             >
-                                                                Cancelar
+                                                                {t("tab1-button")}
                                                             </Button>
                                                             <Button type="submit">Guardar Cambios</Button>
                                                         </>
                                                     ) : (
                                                         <Button type="button" onClick={() => setIsEditing(true)}>
-                                                            Editar Perfil
+                                                            {t("tab1-button")}
                                                         </Button>
                                                     )}
                                                 </div>
@@ -272,8 +276,8 @@ export default function Profile() {
                             <TabsContent value="security">
                                 <Card>
                                     <CardHeader>
-                                        <CardTitle>Seguridad</CardTitle>
-                                        <CardDescription>Actualiza tu contraseña para mantener tu cuenta segura.</CardDescription>
+                                        <CardTitle>{t("tab2-tittle")}</CardTitle>
+                                        <CardDescription>{t("tab2-description")}</CardDescription>
                                     </CardHeader>
                                     <CardContent>
                                         <form onSubmit={handlePasswordUpdate}>
@@ -281,7 +285,7 @@ export default function Profile() {
                                                 <div className="space-y-2">
                                                     <Label htmlFor="currentPassword" className="flex items-center gap-2">
                                                         <Lock className="h-4 w-4" />
-                                                        Contraseña Actual
+                                                        {t("tab2-currentPassword")}
                                                     </Label>
                                                     <Input
                                                         id="currentPassword"
@@ -298,7 +302,7 @@ export default function Profile() {
                                                 <div className="space-y-2">
                                                     <Label htmlFor="newPassword" className="flex items-center gap-2">
                                                         <Lock className="h-4 w-4" />
-                                                        Nueva Contraseña
+                                                        {t("tab2-newPassword")}
                                                     </Label>
                                                     <Input
                                                         id="newPassword"
@@ -313,7 +317,7 @@ export default function Profile() {
                                                 <div className="space-y-2">
                                                     <Label htmlFor="confirmPassword" className="flex items-center gap-2">
                                                         <Lock className="h-4 w-4" />
-                                                        Confirmar Nueva Contraseña
+                                                        {t("tab2-confirmPassword")}
                                                     </Label>
                                                     <Input
                                                         id="confirmPassword"
@@ -326,7 +330,7 @@ export default function Profile() {
                                                 </div>
 
                                                 <div className="flex justify-end pt-4">
-                                                    <Button type="submit">Actualizar Contraseña</Button>
+                                                    <Button type="submit">{t("tab2-button")}</Button>
                                                 </div>
                                             </div>
                                         </form>
