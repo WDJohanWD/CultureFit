f8b6af88f5090bb2f44a95133bfe07522edfa18b
Esteban Gonzalez <estebangp2003@gmail.com>
2025-04-17 10:16:53 +0200

    Select de ejercicios y placeholder si no hay datos

--- backend/culturefit/src/main/java/com/culturefit/culturefit/repositories/ExerciseRepository.java
+++ backend/culturefit/src/main/java/com/culturefit/culturefit/repositories/ExerciseRepository.java
@@ -1,5 +1,7 @@
 package com.culturefit.culturefit.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,5 @@ import com.culturefit.culturefit.domains.Exercise;
 
 @Repository
 public interface ExerciseRepository extends JpaRepository<Exercise, Long>{
-    
+    List<Exercise> findAllByOrderByIdAsc();
 }
--- frontend/src/sections/YourProgress.jsx
+++ frontend/src/sections/YourProgress.jsx
@@ -13,6 +13,7 @@ import { useState } from "react";
 
 function YourProgress() {
   const [graphData, setGraphData] = useState([]);
+  const [exerciseList, setExerciseList] = useState([]);
   const { user } = useContext(AuthContext);
 
   const chartConfig = {
@@ -39,16 +40,26 @@ function YourProgress() {
     setGraphData(fetchedData);
   }
 
+  async function getExercises() {
+    const response = await fetch(`http://localhost:9000/exercise`);
+    if (!response.ok) {
+      throw new Error("An error ocurred while fetching");
+    }
+    const data = await response.json();
+    setExerciseList(data);
+  }
+
   useEffect(() => {
     obtainData(1);
+    getExercises();
   }, []);
 
-  const handleChange = async (event) => {
+  const handleChange = (event) => {
     const idExercise = event.target.value;
     obtainData(idExercise);
   };
 
-  // TODO: Actualizar el select para recibir todoslos ejercicios de la BBDD y añadir que puedas poner tus propios PRs
+  // TODO: Añadir que puedas poner tus propios PRs
 
   return (
     <div className="mx-auto my-10 w-120 sm:w-150 md:w-200 lg:w-240 xl:w-240">
@@ -57,66 +68,77 @@ function YourProgress() {
         className="bg-gray-50 mb-3 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block w-50 p-2"
         onChange={handleChange}
       >
-        <option value="1" default>
-          Press Banca
-        </option>
-        <option value="2">Squat</option>
+        {exerciseList.map((exercise) => (
+          <option key={exercise.id} value={exercise.id}>
+            {exercise.name}
+          </option>
+        ))}
       </select>
 
-      <Card className="">
-        <CardContent>
-          <ChartContainer config={chartConfig}>
-            <LineChart
-              accessibilityLayer
-              data={graphData}
-              margin={{
-                left: 12,
-                right: 12,
-              }}
-            >
-              <CartesianGrid vertical={false} />
-              <XAxis
-                dataKey="date"
-                tickLine={true}
-                axisLine={true}
-                tickMargin={8}
-                tickFormatter={(value) => ""}
-              />
-              <YAxis
-                domain={["auto", "auto"]}
-                tickLine={false}
-                axisLine={false}
-              />
-              <ChartTooltip
-                cursor={false}
-                trigger="item"
-                content={<ChartTooltipContent />}
-                isAnimationActive={false}
-                filterNull={true}
-                activeDot={{ r: 8 }}
-              />
-              <Line
-                dataKey="weight"
-                type="natural"
-                stroke="gray"
-                strokeWidth={1}
-                animationDuration={300}
-                dot={{
-                  stroke: "var(--dot-light-color)",
-                  fill: "var(--dot-light-color)",
-                  r: 4,
+      {graphData.length === 0 ? (
+        <Card className="w-full h-max">
+          <CardContent className="flex items-center justify-center h-60.5 sm:h-77.5 md:h-105.5 lg:h-128">
+            <h1 className="text-primary uppercase montserrat font-semibold text-center text-2xl">
+              You haven't started your progress in this exercise
+            </h1>
+          </CardContent>
+        </Card>
+      ) : (
+        <Card className="">
+          <CardContent>
+            <ChartContainer config={chartConfig}>
+              <LineChart
+                accessibilityLayer
+                data={graphData}
+                margin={{
+                  left: 12,
+                  right: 12,
                 }}
-                activeDot={{
-                  stroke: "var(--dot-color)",
-                  fill: "var(--dot-color)",
-                  strokeWidth: 2,
-                  r: 5,
-                }}
-              />
-            </LineChart>
-          </ChartContainer>
-        </CardContent>
-      </Card>
+              >
+                <CartesianGrid vertical={false} />
+                <XAxis
+                  dataKey="date"
+                  tickLine={true}
+                  axisLine={true}
+                  tickMargin={8}
+                  tickFormatter={(value) => ""}
+                />
+                <YAxis
+                  domain={["auto", "auto"]}
+                  tickLine={false}
+                  axisLine={false}
+                />
+                <ChartTooltip
+                  cursor={false}
+                  trigger="item"
+                  content={<ChartTooltipContent />}
+                  isAnimationActive={false}
+                  filterNull={true}
+                  activeDot={{ r: 8 }}
+                />
+                <Line
+                  dataKey="weight"
+                  type="natural"
+                  stroke="gray"
+                  strokeWidth={1}
+                  animationDuration={300}
+                  dot={{
+                    stroke: "var(--dot-light-color)",
+                    fill: "var(--dot-light-color)",
+                    r: 4,
+                  }}
+                  activeDot={{
+                    stroke: "var(--dot-color)",
+                    fill: "var(--dot-color)",
+                    strokeWidth: 2,
+                    r: 5,
+                  }}
+                />
+              </LineChart>
+            </ChartContainer>
+          </CardContent>
+        </Card>
+      )}
     </div>
   );
 }
