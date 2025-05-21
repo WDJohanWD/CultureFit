7018fa92b4a8e5a28e8dfd19c2dd3ab079d2754a
Esteban Gonzalez <estebangp2003@gmail.com>
2025-05-21 15:37:26 +0200

    NavBar más limpio

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -28,7 +28,6 @@ function NavBar() {
 
   const handleLogout = () => {
     logout();
-
   };
 
   useClickOutside(navbarRef, () => {
@@ -37,7 +36,7 @@ function NavBar() {
 
   return (
     <nav
-      className="bg-primary sticky shadow-lg relative z-50 py-4 px-4 top-0 text-white montserrat font-medium"
+      className="bg-primary sticky shadow-lg z-50 py-4 px-4 top-0 text-white montserrat font-medium"
       ref={navbarRef}
     >
       <div className="flex justify-between align-middle">
@@ -52,49 +51,30 @@ function NavBar() {
               <h2 className="text-4xl montserrat font-semibold">CultureFit</h2>
             </div>
           </Link>
-          <div className="hidden lg:flex">
-            <Link className="pt-1 ms-9 hover:underline">{t("clases")}</Link>
+          <div className="hidden lg:flex ms-9 gap-x-6">
+            <Link to="/memberships" className="pt-1 hover:underline">
+              {t("planes")}
+            </Link>
+            <Link to="/aboutus" className="pt-1 hover:underline">
+              {t("about")}
+            </Link>
+            <Link className="pt-1 hover:underline">{t("clases")}</Link>
             {user ? (
-              <>
-              <Link
-                className="block ms-6 py-1 hover:underline"
-                to="/your-progress"
-              >
-                {t("progress")}
+              <Link to="/appointment" className="pt-1 hover:underline">
+                {t("appointment")}
               </Link>
-              <Link
-                className="block ms-6 py-1 hover:underline"
-                to="/workout"
-              >
-                {t("workout")}
-              </Link>
-              </>
             ) : (
               <></>
             )}
-            <Link to="/memberships" className="pt-1 ms-6 hover:underline">
-              {t("planes")}
-            </Link>
-            <Link to="/aboutus" className="pt-1 ms-6 hover:underline">
-              {t("about")}
-            </Link>
-
-            {user ? (<Link to="/appointment" className="pt-1 ms-6 hover:underline">
-              {t("appointment")}
-            </Link>) :
-              (
-                <></>
-              )}
 
             {isAdmin && (
-              <Link to="/admin" className="pt-1 ms-6 hover:underline">
+              <Link to="/admin" className="pt-1 hover:underline">
                 Admin
               </Link>
             )}
           </div>
         </div>
 
-        {/* Menu desplegable para el movil */}
         <div className="flex items-center">
           <button
             className="lg:hidden p-2"
@@ -103,79 +83,69 @@ function NavBar() {
             <FaBars className="text-white text-2xl" />
           </button>
         </div>
-        <div className={`
+
+        <div
+          className={`
                           lg:hidden 
                           absolute top-20 left-0 w-full bg-primary 
-                          overflow-hidden 
+                          overflow-hidden
                           transition-all duration-300 ease-in-out 
-                          ${menuOpen
-            ? "max-h-[500px] opacity-100 py-4"
-            : "max-h-0 opacity-0 py-0"
-          }
+                          ${
+                            menuOpen
+                              ? "max-h-[500px] opacity-100 py-4"
+                              : "max-h-0 opacity-0 py-0"
+                          }
                           text-center
                         `}
         >
-          <Link className="block py-2 px-4 hover:underline" to="/">
-            {t("clases")}
-          </Link>
-          {user ? (
-            <>
-            <Link 
-              className="block py-2 px-4 hover:underline"
-              to="/your-progress"
-            >
-              {t("progress")}
+          <div className="flex flex-col gap-y-2">
+            <Link className="block hover:underline" to="/">
+              {t("clases")}
             </Link>
-            <Link
-              className="block py-2 px-4 hover:underline"
-              to="/workout"
-            >
-              {t("workout")}
+            <Link to="/memberships" className="block hover:underline">
+              {t("planes")}
             </Link>
-            </>
-          ) : (
-            <></>
-          )}
-          <Link to="/memberships" className="block py-2 px-4 hover:underline">
-            {t("planes")}
-          </Link>
-          <Link to="/aboutus" className="block py-2 px-4 hover:underline">
-            {t("about")}
-          </Link>
-          {user ? (<Link to="/appointment" className="pt-1 ms-6 hover:underline">
-              {t("appointment")}
-            </Link>) :
-              (
-                <></>
-              )}
-          {isAdmin && (
-            <Link to="/admin" className="block py-2 px-4 hover:underline">
-              Admin
+            <Link to="/aboutus" className="block hover:underline">
+              {t("about")}
             </Link>
-          )}
-
-          <div className="items-center align-middle">
             {user ? (
-              <div className="flex flex-col items-center me-2">
-                <Link to="/profile" className="flex flex-col items-center pt-1 hover:underline">
-                  <img src={Profile} alt="Profile" className="h-9 w-12" />
-                  <span className="text-sm">{user.name}</span>
-                </Link>
-              </div>
+              <Link to="/appointment" className="pt-1 hover:underline">
+                {t("appointment")}
+              </Link>
+            ) : (
+              <></>
+            )}
+            {isAdmin && (
+              <Link to="/admin" className="block hover:underline">
+                Admin
+              </Link>
+            )}
+          </div>
 
-            ) : null
-
-            }
+          <div className="items-center align-middle text-center">
             {user ? (
-              <>
-                <button className="text-white bg-light-primary transition hover:ring-3 hover:outline-none hover:ring-orange-400 shadow-lg 
+              <div className="flex flex-row items-center justify-center my-4 gap-x-5">
+                <div className="flex flex-col">
+                  <Link
+                    to="/profile"
+                    className="flex flex-col items-center pt-1 hover:underline"
+                  >
+                    <img src={Profile} alt="Profile" className="h-9 w-12" />
+                    <span className="text-sm">{user.name}</span>
+                  </Link>
+                </div>
+                <button
+                  className="text-white bg-light-primary transition hover:ring-3 hover:outline-none hover:ring-orange-400 shadow-lg 
                                       shadow-red-500/50 dark:shadow-lg font-semibold rounded-lg cursor-pointer
-                                      text-lg px-2 py-2.5 text-center me-4 mb-2 mt-2"
+                                      text-lg px-2 py-2.5 text-center"
                   onClick={handleLogout}
                 >
                   <LuLogOut />
                 </button>
-              </>
+              </div>
+            ) : null}
+            {user ? (
+              <></>
             ) : (
               <div>
                 <button
@@ -195,7 +165,6 @@ function NavBar() {
                     {t("signup")}
                   </button>
                 </Link>
-
               </div>
             )}
 
@@ -223,14 +192,19 @@ function NavBar() {
           {user ? (
             <>
               <div className="flex flex-col items-center me-2">
-                <Link to="/profile" className="pt-1 hover:underline"> <img src={Profile} alt="Profile" className="h-9 w-12" /> </Link>
+                <Link to="/profile" className="pt-1 hover:underline">
+                  {" "}
+                  <img src={Profile} alt="Profile" className="h-9 w-12" />{" "}
+                </Link>
                 <span className="text-sm">{user.name}</span>
               </div>
 
-              <button className="text-white bg-light-primary transition hover:ring-3 hover:outline-none hover:ring-orange-400 shadow-lg 
+              <button
+                className="text-white bg-light-primary transition hover:ring-3 hover:outline-none hover:ring-orange-400 shadow-lg 
                                       shadow-red-500/50 dark:shadow-lg font-semibold rounded-lg cursor-pointer
                                       text-lg px-2 py-2.5 text-center me-4 mb-2 mt-2"
-                onClick={handleLogout}>
+                onClick={handleLogout}
+              >
                 <LuLogOut />
               </button>
             </>
